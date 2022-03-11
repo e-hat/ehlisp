70651690ca478d36b19a197f44959ddeb2168bc2
Eddie Hatfield <edward.hatfield@tufts.edu>
2022-03-10 19:33:17 -0500

    Add vars + lists :)

--- src/parse.rs
+++ src/parse.rs
@@ -2,6 +2,9 @@ use std::collections::VecDeque;
 use std::fmt;
 use std::io;
 use std::io::{Error, ErrorKind};
+use std::str;
+
+use regex::Regex;
 
 pub struct Stream<'a> {
     chars: VecDeque<u8>,
@@ -17,20 +20,37 @@ fn is_digit(c: u8) -> bool {
     c >= b'0' && c <= b'9'
 }
 
+fn is_alpha(c: u8) -> bool {
+    let re = Regex::new(r"[a-zA-Z]").unwrap();
+
+    re.is_match(str::from_utf8(&[c]).unwrap())
+}
+
+fn is_alphanumeric(c: u8) -> bool {
+    let re = Regex::new(r"[a-zA-Z0-9_]").unwrap();
+
+    re.is_match(str::from_utf8(&[c]).unwrap())
+}
+
 fn unexpected(c: u8) -> Error {
-    return Error::new(ErrorKind::InvalidInput, format!("unexpected character: {}", c as char))
+    return Error::new(
+        ErrorKind::InvalidInput,
+        format!("unexpected character: {}", c as char),
+    );
 }
 
 #[derive(Debug)]
 pub enum Ast {
     Fixnum(i32),
     Bool(bool),
+    Local(String),
+    List(Vec<Ast>),
 }
 
 impl fmt::Display for Ast {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Ast::Fixnum(n) => f.write_str(&String::from(format!("{}", n))),
+            Ast::Fixnum(n) => f.write_str(&format!("{}", n)),
             Ast::Bool(b) => {
                 if *b {
                     f.write_str("#t")
@@ -38,6 +58,20 @@ impl fmt::Display for Ast {
                     f.write_str("#f")
                 }
             }
+            Ast::Local(l) => f.write_str(l),
+            Ast::List(elems) => {
+                let mut output = String::from("( ");
+                for (i, e) in elems.iter().enumerate() {
+                    output.push_str(&format!("{}", e));
+                    if i < elems.len() - 1 {
+                        output.push_str(" . ");
+                    }
+                }
+
+                output.push_str(" )");
+
+                f.write_str(&output)
+            },
         }
     }
 }
@@ -57,15 +91,41 @@ impl Stream<'_> {
         let c = self.read_char()?;
         if is_digit(c) || c == b'-' {
             self.unread_char(c);
-            let n = self.read_num()?;
-            Ok(Ast::Fixnum(n))
+            self.read_num().map(|n| Ast::Fixnum(n))
         } else if c == b'#' {
             self.unread_char(c);
-            let b = self.read_bool()?;
-            Ok(Ast::Bool(b))
+            self.read_bool().map(|b| Ast::Bool(b))
+        } else if is_alpha(c) {
+            self.unread_char(c);
+            self.read_id().map(|l| Ast::Local(l))
+        } else if c == b'(' {
+            self.read_list().map(|l| Ast::List(l))
         } else {
             Err(unexpected(c))
         }
+    }
+
+    fn read_list(&mut self) -> io::Result<Vec<Ast>> {
+        self.eat_whitespace()?;
+
+        let c = self.read_char()?;
+        if c == b')' {
+            Ok(Vec::new())
+        } else {
+            self.unread_char(c);
+            let car = self.read_sexp()?;
+            let mut result = vec![car];
+            let mut cdr = self.read_list()?;
+
+            result.append(&mut cdr);
+            Ok(result)
+        }
+    }
+
+    fn read_id(&mut self) -> io::Result<String> {
+        let re = Regex::new(r"^[a-zA-Z][a-zA-Z0-9_]*$").unwrap();
+
+        self.expect_pattern(&re)
     }
 
     fn read_bool(&mut self) -> io::Result<bool> {
@@ -82,13 +142,30 @@ impl Stream<'_> {
         }
     }
 
+    fn expect_pattern(&mut self, re: &Regex) -> io::Result<String> {
+        let mut c = self.read_char()?;
+        let mut input = String::new();
+        input.push(c as char);
+
+        while re.is_match(&input) {
+            c = self.read_char()?;
+            input.push(c as char);
+        }
+
+        self.unread_char(input.pop().unwrap() as u8);
+        if input.is_empty() {
+            Err(unexpected(c))
+        } else {
+            Ok(input)
+        }
+    }
+
     fn expect(&mut self, c: u8) -> io::Result<u8> {
         let input = self.read_char()?;
 
         if input == c {
             Ok(c)
-        }
-        else {
+        } else {
             Err(unexpected(input))
         }
     }
